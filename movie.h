862f27cb143cb1a9cd34ae41c9e2e0215cf872dc
jasonxxu1208 <jasonxxu@uw.edu>
2025-03-12 22:30:12 -0700

    Update movie.h

--- movie.h
+++ movie.h
@@ -4,12 +4,15 @@
 #include<string>
 
 using namespace std;
-
+// abstract base class
 class Movie{
     public:
+    // constructors
     Movie();
     Movie(int stock, string director, string title, int year);
+    // destructor
     virtual ~Movie();
+    // functions
     void changeStock(int input);
     virtual void print(ostream& os)const = 0;
     friend ostream& operator<<(ostream& os, const Movie& movie);
@@ -19,10 +22,10 @@ class Movie{
     int getStock() const;
 
     protected:
-    string title;
-    string director;
-    int stock;
-    int year;
+    string title;  // title of the movie
+    string director;  // director of the movie
+    int stock;  // stock of the movie in the store
+    int year;   // release year of the movie
 
 };
 #endif
